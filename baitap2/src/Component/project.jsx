dac7a87759ed492aeaa134455fa349f9527c0de4
thinhquoc212-code <thinhquoc212@gmail.com>
2026-03-28 01:23:48 +0700

    Baitap2

---
+++ baitap2/src/Component/project.jsx
@@ -0,0 +1,13 @@
+function MyProduct ({img, namee, company, button})
+{
+    return(
+        <div>
+            <img src={img} alt={namee} className="icon"/>
+            <h3>{namee}</h3>
+            <p class="role">{company}</p>
+            <br></br>
+            <button class = "more">xem chi tiết</button>
+        </div>
+    );
+};
+export default MyProduct;
